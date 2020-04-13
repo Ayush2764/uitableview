c689a996c743b0fd4717dbbba330cc370823e42b
Ayush2764 <44580131+Ayush2764@users.noreply.github.com>
2020-04-13 12:15:23 -0400

    Add files via upload

---
+++ contactsapp/contactsapp/contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  contactsapp
+//
+//  Created by Ayush Bisht on 2020-04-13.
+//  Copyright © 2020 SoftAuthor. All rights reserved.
+//
+
+import Foundation
+struct Contact {
+    let name:String?
+    let jobTitle:String?
+    let country:String?
+}
